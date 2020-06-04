80f22501057c5ada3a1f9723e45f63a174b308e9
diegocamy <diegocamyy@gmail.com>
2020-06-04 18:11:27 -0300

    agregado express y funcion para mostrar el codigo qr en produccion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const sulla = require('sulla');
 const fse = require('fs-extra');
+const express = require('express');
 require('dotenv').config();
 
 const { ayuda, generos } = require('./mensajes/ayuda');
@@ -8,6 +9,7 @@ const buscarPelicula = require('./utils/peliculas');
 const obtenerVersiculo = require('./utils/versiculo');
 const { reirse } = require('./utils/reacciones');
 const mensajeHidratarse = require('./mensajes/tomarAgua');
+const app = express();
 
 let recordatorioHidratarse = false;
 let idChat = '';
@@ -15,8 +17,26 @@ let idChat = '';
 fse
   .remove('./session/Default/Service Worker/Database/MANIFEST-000001')
   .then(() => {
-    sulla.create().then(client => start(client));
+    // Second create() parameter is the QR callback
+    sulla
+      .create('julioBot', (base64Qr, asciiQR) => {
+        // To log the QR in the terminal
+        console.log(asciiQR);
+
+        // To write it somewhere else in a file
+        exportQR(base64Qr, 'julioQR.png');
+      })
+      .then(client => start(client));
   });
+
+// Writes QR in specified path
+function exportQR(qrCode, path) {
+  qrCode = qrCode.replace('data:image/png;base64,', '');
+  const imageBuffer = Buffer.from(qrCode, 'base64');
+
+  // Creates 'marketing-qr.png' file
+  fse.writeFileSync(path, imageBuffer);
+}
 
 function start(client) {
   //Recordatorio para tomar agua a cada 30 minutos
@@ -107,3 +127,11 @@ function start(client) {
     }
   });
 }
+
+app.get('/', (req, res) => {
+  return res.sendfile('./julioQR.png');
+});
+
+const PORT = process.env.PORT || '5000';
+
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
